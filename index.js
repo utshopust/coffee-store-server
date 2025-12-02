daa86a2dd9f5a754da44d3e7d768492db9781d00
Hasan Ul Kabir Utsho <mcutsho96@gmail.com>
2025-12-02 15:08:01 +0600

    delete a user api done

--- index.js
+++ index.js
@@ -92,6 +92,14 @@ async function run() {
       res.send(result);
     });
 
+    // delete a user
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await usersCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
